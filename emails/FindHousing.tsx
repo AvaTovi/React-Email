55fb894f35f1f4bdbf7b5488d99c4b000324dca2
Ava Tovi <avatovi45@icloud.com>
2025-01-14 21:35:28 -0800

    Introducing the ideal platform for college students to find and rent apartments near their campus without the hassle. section

--- emails/FindHousing.tsx
+++ emails/FindHousing.tsx
@@ -12,96 +12,122 @@ export const SkateAppEmail = () => (
     </Head>
     <Body>
       <div
-        //Reset password class
         style={{
           width: "736px",
-          height: "38px",
-          borderTopLeftRadius: "24px",
-          borderTopRightRadius: "24px",
-          backgroundColor: "black",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          border: "2px solid black",
-          boxSizing: "border-box",
-        }}
-      >
-        <h1
-          style={{
-            width: "134px",
-            height: "24px",
-            borderTop: "7px",
-            borderLeft: "301px",
-            transform: "rotate(0deg)",
-            color: "#FFFFFF",
-            fontFamily: "Roboto, sans-serif",
-            fontWeight: 600,
-            fontSize: "14px",
-            lineHeight: "24px",
-            textAlign: "center",
-            whiteSpace: "nowrap",
-          }}
-        >
-          Reset Your Password
-        </h1>
-      </div>
-
-      <div
-        //Red frame
-        style={{
-          width: "736px",
-          height: "640px",
-          top: "38px",
-          display: "flex",
-          position: "relative",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          backgroundColor: "#FF5A60",
+          height: "2856px",
+          top: "3391px",
+          left: "6336px",
+          borderRadius: "24px",
         }}
       >
         <div
+          //Reset password class
           style={{
-            //ARE YOU READY section
-            width: "93px",
-            height: "240px",
-            top: "149px",
-            left: "322px",
-            transform: "rotate(0deg)",
-            fontFamily: "Roboto, sans-serif",
-            fontWeight: 700,
-            fontSize: "12px",
-            lineHeight: "24px",
-            letterSpacing: "2%",
+            width: "736px",
+            height: "38px",
+            borderTopLeftRadius: "24px",
+            borderTopRightRadius: "24px",
+            backgroundColor: "black",
+            display: "flex",
             alignItems: "center",
-            textAlign: "center",
-            whiteSpace: "nowrap",
-            color: "#FFFFFF",
+            justifyContent: "center",
+            border: "2px solid black",
+            boxSizing: "border-box",
           }}
         >
-          ARE YOU READY
+          <h1
+            style={{
+              width: "134px",
+              height: "24px",
+              borderTop: "7px",
+              borderLeft: "301px",
+              transform: "rotate(0deg)",
+              color: "#FFFFFF",
+              fontFamily: "Roboto, sans-serif",
+              fontWeight: 600,
+              fontSize: "14px",
+              lineHeight: "24px",
+              textAlign: "center",
+              whiteSpace: "nowrap",
+            }}
+          >
+            Reset Your Password
+          </h1>
         </div>
 
         <div
+          //Red frame
           style={{
-            width: "510.63px",
-            height: "132px",
-            top: "235px",
-            left: "113px",
-            fontFamily: "Roboto, sans-serif",
-            fontWeight: 700,
-            fontSize: "48.63px",
-            lineHeight: "66px",
-            textAlign: "center",
-            position: "absolute",
-            //alignItems: "center",
-            //display: "flex",
-            //justifyContent: "center",
-            color: "#FFFFFF",
-            border: "2px solid black",
+            width: "736px",
+            height: "640px",
+            display: "flex",
+            position: "relative",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            backgroundColor: "#FF5A60",
           }}
         >
-          FIND HOUSING NEAR YOUR CAMPUS
+          <div
+            style={{
+              //ARE YOU READY section
+              width: "93px",
+              height: "350px",
+              top: "149px",
+              left: "322px",
+              transform: "rotate(0deg)",
+              fontFamily: "Roboto, sans-serif",
+              fontWeight: 700,
+              fontSize: "12px",
+              lineHeight: "24px",
+              letterSpacing: "2%",
+              alignItems: "center",
+              textAlign: "center",
+              whiteSpace: "nowrap",
+              color: "#FFFFFF",
+            }}
+          >
+            ARE YOU READY
+          </div>
+
+          <div
+            style={{
+              // FIND HOUSING NEAR YOUR CAMPUS section
+              width: "510.63px",
+              height: "132px",
+              top: "183px",
+              left: "113px",
+              fontFamily: "Roboto, sans-serif",
+              fontWeight: 700,
+              fontSize: "48.63px",
+              lineHeight: "66px",
+              textAlign: "center",
+              position: "absolute",
+              color: "#FFFFFF",
+            }}
+          >
+            FIND HOUSING NEAR YOUR CAMPUS
+          </div>
+
+          <div
+            style={{
+              //Introducing the ideal platform for college students to find and rent apartments near their campus without the hassle. section
+              width: "537px",
+              height: "52px",
+              top: "328px",
+              left: "100px",
+              fontFamily: "inter, sans-serif",
+              fontWeight: 400,
+              fontSize: "18px",
+              lineHeight: "26px",
+              textAlign: "center",
+              position: "absolute",
+              color: "#FCFCFC",
+            }}
+          >
+            Introducing the ideal platform for college students to find and{" "}
+            <br /> rent apartments near their campus without the hassle.
+          </div>
         </div>
       </div>
     </Body>
